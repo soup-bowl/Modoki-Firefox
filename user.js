c73a9a8da0d595c84960a629ef6ccfbbf69bb39b
soup-bowl <code@soupbowl.io>
2022-09-26 17:56:07 +0000

    Moved userprefs into a file.

---
+++ user.js
@@ -0,0 +1,5 @@
+// Used by Gitpod or a dev env to setup Firefox for UserStyle debugging.
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.compactmode.show", true);
+user_pref("devtools.chrome.enabled", true);
+user_pref("browser.uidensity", 1);
