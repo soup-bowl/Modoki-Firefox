29a78b6e99defc5410d07b01171b2010f69c84ab
soup-bowl <code@soupbowl.io>
2023-03-19 00:25:38 +0000

    Symlink directives backwards.

--- .gitpod/installation.sh
+++ .gitpod/installation.sh
@@ -8,12 +8,12 @@ sudo sed -i 's/Name=Firefox Web Browser/Name=Firefox Web Browser (ESR)/' /usr/sh
 sudo wget -O /opt/firefox-nightly.tar.bz2 "https://download.mozilla.org/?product=firefox-nightly-latest&os=linux64&lang=en-US"
 sudo tar -xjf /opt/firefox-nightly.tar.bz2 -C /opt
 sudo mv /opt/firefox /opt/firefox-nightly
-sudo ln -sf /opt/firefox/firefox-nightly /usr/local/bin/firefox-nightly
+sudo ln -sf /opt/firefox-nightly/firefox /usr/local/bin/firefox-nightly
 
 sudo wget -O /opt/firefox-developer.tar.bz2 "https://download.mozilla.org/?product=firefox-devedition-latest&os=linux64&lang=en-US"
 sudo tar -xjf /opt/firefox-developer.tar.bz2 -C /opt
 sudo mv /opt/firefox /opt/firefox-developer
-sudo ln -sf /opt/firefox/firefox-developer /usr/local/bin/firefox-developer
+sudo ln -sf /opt/firefox-developer/firefox /usr/local/bin/firefox-developer
 
 timeout 5 firefox
 timeout 5 firefox-esr
