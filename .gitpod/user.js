e7cce0e3c753e9f4c296135f6d24d372f7f9f54a
soup-bowl <code@soupbowl.io>
2024-05-20 07:39:43 +0000

    Moved GP FF install script outside-repo.

--- .gitpod/user.js
+++
@@ -1,8 +0,0 @@
-// Used by Gitpod or a dev env to setup Firefox for UserStyle debugging.
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.compactmode.show", true);
-user_pref("browser.toolbars.bookmarks.visibility", "always");
-user_pref("devtools.debugger.remote-enabled", true);
-user_pref("devtools.chrome.enabled", true);
-user_pref("browser.uidensity", 1);
-user_pref("browser.tabs.inTitlebar", 0);
